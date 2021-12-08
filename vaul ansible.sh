3d66f0b90f95683358888257ff6f85a1261c7b3c
Potasio101 <Portsayco@gmail.com>
2021-12-07 22:14:36 -0700

    addint summary information of vault

--- vaul ansible.sh
+++ vaul ansible.sh
@@ -1,5 +1,30 @@
 Information and Notes to use Vault
 
+Summary: vault to encrypt data for my need I only need encrypt the password from clear text to Ansible
+The following example will show how will see:
+
+ansible_network_os: cisco.ios.ios
+ansible_user: admin
+ansible_password: cisco
+ 
+##########################################################################################
+
+ansible_network_os: cisco.ios.ios
+ansible_user: admin
+ansible_password: !vault |
+          $ANSIBLE_VAULT;1.1;AES256
+          62386336616663366561323533623435353861323230383865623434316663373831353463393536
+          6164653336643530346263656433663264666366623735340a623361623564656265313962363531
+          35313433653337346462323439346435653464623961336338663662323231326230373935313264
+          6465313738353364370a343333646537616230333139643330386462306566343433373861313664
+          3961
+ 
+Before we encrypt the data we need create a strong password and storage that in safe location like ~/.ansible/.vault_key
+After we have or secure password it would be acceptable to also edit the permission access to only be access to a secure group
+After we have the .vault_key we need edit the ansible.cfg "vault_password_file = ~/.ansible/.vault_key" and after we have this-
+the proccess will be automatic to encrypt and dencrypt
+
+
 1. never store the your vault key in github "lol"
 
 Step 1
